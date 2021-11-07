49e0835fe295435be45804d108e79c5ce8f3db0f
stanliwise <stanliwise@gmail.com>
2021-11-07 12:09:37 +0100

    new changes

--- index.php
+++ index.php
@@ -201,7 +201,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Game programming - Tic-tac-toe</title>
-    <link rel="stylesheet" href="assets/css/bootstrap.css" type="text/css">
+    <link rel="stylesheet" href="./assets/css/bootstrap.css" type="text/css">
     <style>
         .table td{
             text-align: center;
